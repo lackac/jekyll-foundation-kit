ba2f4e2732e2c9af99dcb7d28a031914d133066a
Laszlo Bacsi <lackac@lackac.hu>
2014-02-23 13:51:34 +0100

    Upgrade to Foundation 5.1.0 and jQuery 2.1.0

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -4,6 +4,6 @@ require "sprockets"
 
 bower_components = File.expand_path("../bower_components", __dir__)
 
-%w[ foundation/scss foundation/js jquery modernizr ].each do |path|
+%w[ foundation/scss foundation/js jquery/dist modernizr ].each do |path|
   Sprockets.append_path File.join(bower_components, path)
 end
